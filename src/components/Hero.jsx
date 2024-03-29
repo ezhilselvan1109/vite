60778b350a409e21d4c2b1172d59c521a7686409
Ezhil Selvan P <ezhilselvan1109@gmail.com>
2024-03-29 16:48:06 +0530

    second commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Hero({heroapi:{title,subtitle,bntext,img,sociallimks,videos}}) {
+  return (
+    <>
+      <div>
+        <div></div>
+        <div>
+            <div>
+                <h1></h1>
+                <h1></h1>
+                <button></button>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <img src="" alt="hero-img/img" />
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Hero
